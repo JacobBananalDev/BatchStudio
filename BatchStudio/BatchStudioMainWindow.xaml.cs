167cfaac45e84af37a11fdd2af8323f4239f37a6
Jacob Bananal <jacob.bananal@tek84.com>
2025-05-29 09:57:14 -0700

    BatchStudio - Renamed MainWindow to BatchStudioMainWindow

---
+++ BatchStudio/BatchStudioMainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BatchStudio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class BatchStudioMainWindow : Window
+    {
+        public BatchStudioMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
